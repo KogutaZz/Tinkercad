d724a6b6bbf8f4728081cca5466ad53eedef0dd2
KogutaZz <alevqv@hotmail.com>
2026-07-24 11:50:04 -0300

    Create Atividade05.cpp

---
+++ Atividades 24-07/Atividade05.cpp
@@ -0,0 +1,14 @@
+#define botaoA 6
+#define botaoB 7
+#define ledXOR 8
+#define ledOU 9
+#define ledE 10
+
+boolean valA = 0;
+boolean valB = 0;
+
+void setup() {
+  
+  pinMode(ledXOR, OUTPUT);
+  pinMode(ledOU, OUTPUT);
+  pinMode(ledE, OUTPUT);
