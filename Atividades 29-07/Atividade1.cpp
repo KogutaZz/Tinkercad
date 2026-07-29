c1395303571a7783a2b8edae5ed379311a92276e
KogutaZz <alevqv@hotmail.com>
2026-07-29 10:53:53 -0300

    Add temperature reading functionality in Atividade1.cpp

--- Atividades 29-07/Atividade1.cpp
+++ Atividades 29-07/Atividade1.cpp
@@ -1,1 +1,23 @@
+float tempC = 0;
 
+int tempPin = 0;
+
+void setup() {
+  
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+  tempC = analogRead(tempPin);
+  tempC = (5.0 * tempC * 100.0)/1024.0;
+
+
+  Serial.println("Lendo temperatura... ");
+  Serial.print((byte)tempC);
+  Serial.println(" C");
+  
+  delay(1000);
+  
+}
